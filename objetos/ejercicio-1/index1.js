6a100d3565a42c0d34f4cb2c86399904be360dd0
Laura Flórez <lauraflorez520@gmail.com>
2020-03-20 22:46:48 -0500

    primer punto

---
+++ objetos/ejercicio-1/index1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const geekGirl1 = {}
+geekGirl1.nombre = "Susana";
+geekGirl1.edad = 34;
+geekGirl1.profesion = "Periodista";
+
+document.write("Mi nombre es ", geekGirl1.nombre + ", tengo ", geekGirl1.edad + " años" + " y soy", geekGirl1.profesion);
+
+const geekGirl2 = {}
+nombre = "Rocio";
+edad = 25;
+profesion = "Actriz";
+
+document.write("Mi nombre es ", [nombre] + ", tengo ", [edad] + " años" + " y soy " + [profesion]);
